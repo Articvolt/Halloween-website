244733e25343058693aa212972587eeb4e58a679
Ugo DA SILVA <ugodasilva67@gmail.com>
2022-10-30 18:39:40 +0100

    add js and css responsive to navbar (not finished)

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+// SHOW MENU
+
+// définition des constantes à appelées
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose =document.getElementById('nav-close')
