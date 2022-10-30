f88f26d3e13a10012b7fde17673156b05b8817c8
Ugo DA SILVA <ugodasilva67@gmail.com>
2022-10-30 21:09:52 +0100

    add animation Js to navbar menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,28 @@
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose =document.getElementById('nav-close')
+
+// pour afficher la barre de navigation
+if(navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+//pour cacher la barre de navigation
+if(navClose$) {
+    navClose.addEventListener('click' , () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+//REMOVE MENU
+
+//déclaration de la constante
+const navLink = document.querySelectorAll('.nav__link')
+
+// cache le menu après avoir cliqué sur un lien
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+}
+navLink.forEach(n => n.addEventListener('click' , linkAction))
