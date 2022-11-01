52d7cca317486d933e0764a1d0b2956203587138
Ugo DA SILVA <ugodasilva67@gmail.com>
2022-11-01 14:43:39 +0100

    debug some code (js and css)

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,18 +3,18 @@
 // définition des constantes à appelées
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
-      navClose =document.getElementById('nav-close')
+      navClose = document.getElementById('nav-close')
 
 // pour afficher la barre de navigation
 if(navToggle) {
-    navToggle.addEventListener('click', () => {
+    navToggle.addEventListener('click', () =>{
         navMenu.classList.add('show-menu')
     })
 }
 
 //pour cacher la barre de navigation
-if(navClose$) {
-    navClose.addEventListener('click' , () => {
+if(navClose) {
+    navClose.addEventListener('click', () =>{
         navMenu.classList.remove('show-menu')
     })
 }
@@ -25,10 +25,11 @@ if(navClose$) {
 const navLink = document.querySelectorAll('.nav__link')
 
 // cache le menu après avoir cliqué sur un lien
-const linkAction = () => {
+const linkAction = () =>{
     const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
 }
-navLink.forEach(n => n.addEventListener('click' , linkAction))
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 // CHANGE BACKGROUND HEADER
